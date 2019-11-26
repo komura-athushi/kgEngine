2d1e478893061f17b1e7eb685c44dd554ee71ffc
komura athushi <kbc18b09@kawahara.ac.jp>
2019-11-26 15:48:29 +0900

    addし忘れ

---
+++ GameTemplate/Game/Collection.cpp
@@ -0,0 +1,96 @@
+#include "stdafx.h"
+#include "Collection.h"
+
+Collection::Collection()
+{
+
+}
+
+Collection::~Collection()
+{
+
+}
+
+bool Collection::Start()
+{
+
+	m_offRenderTarget.Create(FRAME_BUFFER_W, FRAME_BUFFER_H, DXGI_FORMAT_R16G16B16A16_FLOAT);
+	//m_skinModel.Init(L"Resource/modelData/zunko.cmo");
+	m_vs.Load("Assets/shader/bloom.fx", "VSMain", Shader::EnType::VS);
+	m_ps.Load("Assets/shader/bloom.fx", "PSMain", Shader::EnType::PS);
+	D3D11_RASTERIZER_DESC desc = {};
+	desc.CullMode = D3D11_CULL_NONE;
+	desc.FillMode = D3D11_FILL_SOLID;
+	desc.DepthClipEnable = true;
+	desc.MultisampleEnable = true;
+
+	//ラスタライザとビューポートを初期化。
+	Engine().GetGraphicsEngine().GetD3DDevice()->CreateRasterizerState(&desc, &m_rasterizerState);
+	InitSamplerState();
+	for (int i = 0; i <= 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			m_postEffect[i * 4 + j].InitScreenQuadPrimitive(CVector2(-1.0f + 0.5f * j, 0.5f - 0.5f * i), CVector2(-0.5f + 0.5f * j, 0.5f - 0.5f * i), CVector2(-1.0f + 0.5f * j, 1.0f - 0.5f * i), CVector2(-0.5f + 0.5f * j, 1.0f - 0.5f * i));
+		}
+	}
+	
+	m_skinModel = new SkinModel();
+	m_skinModel->Init(L"Resource/modelData/zunko.cmo");
+
+	m_modelList = ObjectData::GetInstance().GetList();
+	return true;
+}
+
+void Collection::InitSamplerState()
+{
+	//テクスチャのサンプリング方法を指定するためのサンプラステートを作成。
+	D3D11_SAMPLER_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	desc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
+	desc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
+	desc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
+	desc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	Engine().GetGraphicsEngine().GetD3DDevice()->CreateSamplerState(&desc, &m_samplerState);
+}
+
+void Collection::Draw()
+{
+
+	m_offScreenCamera.Update();
+
+	auto d3dDeviceContext = Engine().GetGraphicsEngine().GetD3DDeviceContext();
+
+	int i = 0;
+	for (auto itr : m_modelList) {
+		if (i == 16) {
+			return;
+		}
+		//レンダリングターゲットを切り替える。
+		ID3D11RenderTargetView* rts[] = {
+			m_offRenderTarget.GetRenderTargetView()
+		};
+		d3dDeviceContext->OMSetRenderTargets(1, rts, m_offRenderTarget.GetDepthStensilView());
+		//ビューポートを設定。
+		d3dDeviceContext->RSSetViewports(1, m_offRenderTarget.GetViewport());
+		d3dDeviceContext->RSSetState(m_rasterizerState);
+		//一番奥のZは1.0なので、1.0で塗りつぶす。
+		float clearColor[4] = { 0.7f, 0.7f, 0.7f, 1.0f }; //red,green,blue,alpha
+		m_offRenderTarget.ClearRenderTarget(clearColor);
+
+		if (itr.second->s_isHit) {
+			itr.second->s_skinModel.UpdateWorldMatrix(m_position, m_rot, m_scale);
+			itr.second->s_skinModel.Draw(m_offScreenCamera.GetViewMatrix(), m_offScreenCamera.GetProjectionMatrix());
+		}
+
+
+		Engine().GetGraphicsEngine().ChangeRenderTarget(Engine().GetGraphicsEngine().GetMainRenderTarget(), Engine().GetGraphicsEngine().GetMainRenderTarget()->GetViewport());
+		d3dDeviceContext->PSSetSamplers(0, 1, &m_samplerState);
+		//シーンをテクスチャとする。
+		auto srv = m_offRenderTarget.GetRenderTargetSRV();
+		d3dDeviceContext->PSSetShaderResources(0, 1, &srv);
+		m_postEffect[i].DrawFullScreenQuadPrimitive(d3dDeviceContext, m_vs, m_ps);
+		ID3D11ShaderResourceView* s[] = { NULL };
+		d3dDeviceContext->PSSetShaderResources(0, 1, s);
+		i++;
+	}
+}
+
---
+++ GameTemplate/Game/Collection.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "graphics/RenderTarget.h"
+#include "OffScreenCamera.h"
+#include "Object/ObjectData.h"
+
+class Collection : public IGameObject
+{
+public:
+	Collection();
+	~Collection();
+	bool Start() override;
+	void Draw() override;
+	void SetSkinModel(SkinModel* skinModel)
+	{
+		m_skinModel = skinModel;
+	}
+
+	void InitSamplerState();
+private:
+	RenderTarget m_offRenderTarget;
+	OffScreenCamera m_offScreenCamera;
+	SkinModel* m_skinModel = nullptr;
+	CVector3 m_position = CVector3::Zero();
+	CVector3 m_scale = CVector3::One();
+	CQuaternion m_rot = CQuaternion::Identity();
+	Shader m_vs;
+	Shader m_ps;
+	ID3D11SamplerState* m_samplerState = nullptr;		//サンプラステート。
+	PostEffect m_postEffect[16];
+	ID3D11RasterizerState* m_rasterizerState = NULL;	//ラスタライザステート。
+	std::map<int, SkinModel_ObjData*> m_modelList;
+};
+
